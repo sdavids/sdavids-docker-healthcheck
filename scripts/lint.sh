d9a2d3e2bf0a37cb85999c6035bcfd5fbfe7c946
Sebastian Davids <sdavids@gmx.de>
2022-01-22 08:56:19 +0100

    chore: initial
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# script needs to be invoked from the project's root directory
+
+set -eu
+
+#readonly base_dir="$PWD"

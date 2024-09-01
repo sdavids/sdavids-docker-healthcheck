5ee8fdf6910d84f24bf146d852298893225ea5d3
Sebastian Davids <sdavids@gmx.de>
2024-09-02 00:08:47 +0200

    feat: rust - http
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ rust/http/scripts/docker_sh.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-docker-healthcheck-rust-http'
+
+docker exec \
+  --interactive \
+  --tty \
+  "${container_name}" \
+  sh

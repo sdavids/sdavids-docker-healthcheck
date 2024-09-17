bc93b07c506d417f1de797423cd23942fb6ba427
Sebastian Davids <sdavids@gmx.de>
2024-09-17 17:14:44 +0200

    feat: java - http
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/format_check.sh
+++ scripts/format_check.sh
@@ -11,6 +11,7 @@ readonly base_dir="$PWD"
 
 go/http/scripts/format_check.sh "${base_dir}/go/http"
 go/https/scripts/format_check.sh "${base_dir}/go/https"
+java/http/scripts/format_check.sh "${base_dir}/java/http"
 js/nodejs/scripts/format_check.sh "${base_dir}/js/nodejs"
 rust/http/scripts/format_check.sh "${base_dir}/rust/http"
 rust/https/scripts/format_check.sh "${base_dir}/rust/https"

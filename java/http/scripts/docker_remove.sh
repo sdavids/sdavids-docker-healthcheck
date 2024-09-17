570aa41ecd31a11d7fc9b24afff32d63cec172da
Sebastian Davids <sdavids@gmx.de>
2024-09-17 17:14:44 +0200

    feat: java - http
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ java/http/scripts/docker_remove.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-docker-healthcheck-java-http'
+
+if [ -n "$(docker container ls --all --quiet --filter="name=^/${container_name}$")" ]; then
+  docker container stop "${container_name}"
+fi
+
+# container not started with --rm ?
+if [ -n "$(docker container ls --all --quiet --filter="name=^/${container_name}$")" ]; then
+  docker container remove --force --volumes "${container_name}"
+fi
+
+readonly network_name='sdavids_docker_healthcheck'
+
+if docker network inspect "${network_name}" >/dev/null 2>&1; then
+  docker network rm "${network_name}" >/dev/null
+fi

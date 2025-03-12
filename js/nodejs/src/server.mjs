87cc5931402181ac2c29b5a78a47e4fe5f3140ca
Sebastian Davids <sdavids@gmx.de>
2025-03-12 18:24:24 +0100

    feat: improve nodejs server
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- js/nodejs/src/server.mjs
+++ js/nodejs/src/server.mjs
@@ -1,36 +1,58 @@
 // SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
 // SPDX-License-Identifier: Apache-2.0
 
-import { readFileSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 import process from 'node:process';
 
-['uncaughtException', 'unhandledRejection'].forEach((s) =>
-  process.once(s, (e) => {
-    console.error(e);
+['uncaughtException', 'unhandledRejection'].forEach((signal) =>
+  process.on(signal, (err) => {
+    console.error(err);
     process.exit(70); // EX_SOFTWARE
   }),
 );
-['SIGINT', 'SIGTERM'].forEach((s) => process.once(s, () => process.exit(0)));
+['SIGINT', 'SIGTERM'].forEach((signal) =>
+  process.on(signal, () => process.exit(0)),
+);
 
-const port = 3000;
+// eslint-disable-next-line dot-notation
+const port = Number(process.env['PORT'] ?? 3000);
+if (isNaN(port) || port < 1 || port > 65535) {
+  console.error(`Illegal port '${port}' [1..65535]`);
+  process.exit(64); // EX_USAGE
+}
+// eslint-disable-next-line dot-notation
+const keyPath = process.env['KEY_PATH'] ?? 'key.pem';
+// eslint-disable-next-line dot-notation
+const certPath = process.env['CERT_PATH'] ?? 'cert.pem';
 
-let options = {};
+let secure = keyPath && certPath;
+if (secure) {
+  secure = existsSync(certPath) && existsSync(keyPath);
+}
+
+const options = {};
 // eslint-disable-next-line init-declarations
 let engine;
-try {
-  const key = readFileSync('key.pem');
-  const cert = readFileSync('cert.pem');
+if (secure) {
+  try {
+    options.cert = readFileSync(certPath);
+  } catch {
+    console.error(`certificate '${certPath}' not found`);
+    process.exit(64); // EX_USAGE
+  }
+  try {
+    options.key = readFileSync(keyPath);
+  } catch {
+    console.error(`key '${keyPath}' not found`);
+    process.exit(64); // EX_USAGE
+  }
   try {
     engine = await import('node:https');
   } catch {
     console.error('https support is disabled');
     process.exit(78); // EX_CONFIG
   }
-  options = {
-    key,
-    cert,
-  };
-} catch {
+} else {
   engine = await import('node:http');
 }
 
@@ -47,11 +69,34 @@ const server = engine.createServer(options, ({ url }, res) => {
     res.writeHead(404).end('Not found');
   }
 });
-server.keepAliveTimeout = 5000;
-server.requestTimeout = 5000;
-server.timeout = 5000;
-server.listen(port);
 
-console.log(
-  `Listen local: http${Object.hasOwn(options, 'key') ? 's' : ''}://localhost:${port}`,
+server.keepAliveTimeout = 5000;
+server.requestTimeout = 10000;
+server.timeout = 15000;
+
+server.listen(port, () =>
+  console.log(`Listen local: http${secure ? 's' : ''}://localhost:${port}`),
 );
+
+server.on('timeout', (socket) => {
+  socket.destroy();
+});
+
+server.on('error', (err) => {
+  if (err.syscall === 'listen') {
+    switch (err.code) {
+      case 'EACCES':
+        console.error('Port requires elevated privileges');
+        process.exit(77); // EX_NOPERM
+        break;
+      case 'EADDRINUSE':
+        console.error('Port is already in use');
+        process.exit(75); // EX_TEMPFAIL
+        break;
+      default:
+      // just log
+    }
+  }
+
+  console.error(err);
+});
